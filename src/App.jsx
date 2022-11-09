acadc959bfd7dfc5f32bc3382cdaca7c09a0a269
Jorge Silva <89113954+jorgehpsilva@users.noreply.github.com>
2022-11-08 21:15:13 -0300

    Evolução da 1ª a 7ª aula

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { InstagramTimeLine } from './challenges/InstagramTimeLine'
+import { QuartaAula } from './lessons/QuartaAula'
+import { QuintaAula } from './lessons/QuintaAula'
+import { TerceiraAula } from './lessons/TerceiraAula'
+import { DHGames } from './pages/DHGames'
+import { SetimaAula } from './lessons/SetimaAula'
+
+function App() {
+
+  return (
+    <SetimaAula />
+  )
+
+}
+
+export default App
